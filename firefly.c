fb6a0505cc02b11ce2303d1c7b3148506ee952f5
Colin Meyer <cmeyer@helvella.org>
2013-08-14 00:05:05 -0700

    switched to count method; gained consistency!

--- firefly.c
+++ firefly.c
@@ -27,8 +27,9 @@
 #define FF1_MASK (1<<FF1_MALE | 1<<FF1_FEMALE)
 
 
-// light meter returns true when it's dark out
-uint8_t light_is_low_enough() {
+// light returns count of ADC samples for LED capacitor to reach zero
+//   count < 30 means light out
+uint8_t light_detect() {
     // read ADC2 input
     ADMUX = 2;              // Select channel ADC = 2
     ADCSRA |= (1<<ADEN)     // turn on ADC
@@ -42,8 +43,6 @@ uint8_t light_is_low_enough() {
 
     uint8_t   adcl;
     uint8_t   adch;
-    uint16_t  first_measure  = 0;
-    uint16_t  second_measure = 0;
 
     // charge detector LED briefly (it is a capacitor)
     DDRB  =  (1<<DDB4); // select pin 3 for output
@@ -56,59 +55,30 @@ uint8_t light_is_low_enough() {
     PORTB &= ~(1<<FF1_MALE_ ); // set pin to low (switch pull-up resistor off)
                          // now pin is tri-state
 
-    ADCSRA |= (1 << ADSC);        // start single conversion
-    while (ADCSRA & (1 << ADSC)); // wait until conversion is done
+    uint8_t c = 0;
+    do {
+        c++;
+        ADCSRA |= (1 << ADSC);        // start single conversion
+        while (ADCSRA & (1 << ADSC)); // wait until conversion is done
 
-    // take first voltage measurement
-    adcl = ADCL;
-    adch = ADCH;
-    first_measure = adch << 8 | adcl;
+        // take first voltage measurement
+        adcl = ADCL;
+        adch = ADCH;
+    }
+    while ( adch > 0 && adcl > 0x10 );
 
-    // delay some constant time amount, while LED discharges
-    _delay_us(MEASURE_GAP_DELAY);
-
-    ADCSRA |= (1 << ADSC);        // start single conversion
-    while (ADCSRA & (1 << ADSC)); // wait until conversion is done
-
-    adcl = ADCL;
-    adch = ADCH;
-    second_measure = adch << 8 | adcl;
-
-    // turn off ADC
-    ADCSRA &= ~(1<<ADEN);
-
-    // calculate the diff between the two -- the amount that the capacitor has discharged
-    // bright shining on the LED makes it discharge quicker
-    // so the second measure will be lower
-    // causing the displayed number (diff) to be higher
-    uint16_t diff;
-    if ( second_measure > first_measure )
-        diff = 0;
-    else
-        diff = first_measure - second_measure;
-
-    static uint8_t write_address = 0;
-//     eeprom_update_word((uint16_t *)(write_address), first_measure);
-//     write_address += 2;
-// 
-//     eeprom_update_word((uint16_t *)(write_address), second_measure);
-//     write_address += 2;
-
-    eeprom_update_word((uint16_t *)(write_address), diff);
-    write_address += 2;
-
-    // smaller is darker; function returns true for dark
-    return (diff < 0x0300) ? 1 : 0;
+    return c;
 }
 
 
 
 int main(void)
 {
-
     uint8_t c;
-    for ( c = 0; c < 64/2; c++ ) { // writing four bytes each call it light_is_low_enough()
-        light_is_low_enough();
+    for ( c = 0; c < 64; c++ ) { // writing four bytes each call it light_is_low_enough()
+        uint8_t light;
+        light = light_detect();
+        eeprom_update_byte( (uint8_t *)c, light );
         _delay_ms(100);
     }
 
